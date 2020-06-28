e9b78ca839f75c961bedf8b265d859be0766f203
taiz <walter.kerly@protonmail.com>
2020-06-28 07:22:40 -0400

    Add UI elements

--- src/player.lua
+++ src/player.lua
@@ -7,10 +7,12 @@ Player = {
 	},
 	height = 25,
 	width = 25,
-	health = 50
+	health = 50,
+	money = 0
 }
 
 function Player.draw(this)
+	-- Figure out offsets depending on the direction the player is facing
 	if (not this.forwardFace) then
 		this.scaleX = -3
 		this.scaleY = 3
@@ -20,8 +22,18 @@ function Player.draw(this)
 		this.offsetX = 5
 		this.offsetY = 5
 	end
+
+	-- Draw the player to the screen
 	love.graphics.setColor(1, 1, 1, 1)
-		love.graphics.draw(this.texture, Camera.convert("x", this.x), Camera.convert("y", this.y), this.rotation, this.scaleX, this.scaleY, this.offsetX, this.offsetY)
+	love.graphics.draw(this.texture, Camera.convert("x", this.x), Camera.convert("y", this.y), this.rotation, this.scaleX, this.scaleY, this.offsetX, this.offsetY)
 	--love.graphics.rectangle("fill", Camera.convert("x", this.x), Camera.convert("y", this.y), this.width, this.height)
+
+	-- Draw the UI after
+	Player.drawUI()
+end
+
+function Player.drawUI()
+	love.graphics.print("Health: " .. Heartbeat.player.health)
+	love.graphics.print("\n/G/old:" .. Player.money)
 end
 
