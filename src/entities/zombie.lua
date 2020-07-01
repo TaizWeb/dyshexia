7770d0b94e8c9b5768aca396e4c2655b34197e5d
taiz <walter.kerly@protonmail.com>
2020-07-01 14:00:00 +0100

    Add zombie entity

---
+++ src/entities/zombie.lua
@@ -0,0 +1,54 @@
+Zombie = {
+	id = "zombie",
+	--texture = love.graphics.newImage("assets/entities/zombie/zombie-front.png"),
+	textures = {
+		--front = love.graphics.newImage("assets/entities/zombie/zombie-front.png"),
+		--back = love.graphics.newImage("assets/entities/zombie/zombie-back.png"),
+		--side = love.graphics.newImage("assets/entities/zombie/zombie-side.png")
+	},
+	height = 25,
+	width = 25,
+	scaleX = 3,
+	scaleY = 3,
+	health = 10,
+	isEnemy = true
+}
+
+function Zombie.draw(this)
+	if (not this.forwardFace) then
+		this.scaleX = -3
+	else
+		this.scaleX = 3
+	end
+	Heartbeat.draw(this)
+end
+
+function Zombie.behaivor(this)
+	-- Later I'll do a thing where it checks the player's x/y and moves depending on that
+	local diffX = Heartbeat.player.x - this.x
+	local diffY = Heartbeat.player.y - this.y
+	if (isAdjacent(this, Heartbeat.player)) then
+		Zombie.attack(this)
+	elseif (math.abs(diffX) > math.abs(diffY)) then
+		if (diffX < 0) then
+			moveEntity(this, "left")
+		else
+			moveEntity(this, "right")
+		end
+	else
+		if (diffY < 0) then
+			moveEntity(this, "up")
+		else
+			moveEntity(this, "down")
+		end
+	end
+end
+
+function Zombie.attack(this)
+	print("Rawr xd")
+end
+
+function Zombie.onDeath(this)
+	-- Make it drop something, grant exp, inflict a debuff, etc
+end
+
