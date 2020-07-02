6298895eb0376c582b445f00696b6d528e1de524
taiz <walter.kerly@protonmail.com>
2020-07-02 18:45:36 -0400

    Add a non-collidable ground

--- src/tiles.lua
+++ src/tiles.lua
@@ -5,3 +5,13 @@ Wall = {
 	height = 25,
 	isSolid = true
 }
+
+-- There was approximately ZERO effort in this texture lmao
+Ground = {
+	id = "ground",
+	texture = love.graphics.newImage("assets/tiles/ground.png"),
+	width = 25,
+	height = 25, 
+	isSolid = false
+}
+
