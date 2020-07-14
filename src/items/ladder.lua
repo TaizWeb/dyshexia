73dcc80ca765b16ac0213f477e57ce69e9abd119
taiz <walter.kerly@protonmail.com>
2020-07-14 01:43:42 -0400

    Make ladder generate a new level instead of using prebuilt ones

--- src/items/ladder.lua
+++ src/items/ladder.lua
@@ -8,23 +8,12 @@ Ladder = {
 }
 
 function Ladder.onPickup(this)
-	Heartbeat.player.health = 50
+	Heartbeat.player.health = 200
+	-- Clear all items/entities/tiles
 	Heartbeat.clear()
+	-- Reset stuff like Level.roomCount
+	Level.clear()
+	Level.generateLevel()
 	Player.currentLevel = Player.currentLevel + 1
-	Heartbeat.editor.readLevel("level" .. Player.currentLevel)
-	-- I'm hardcoding shit because levelgen isn't a thing yet
-	if (Player.currentLevel == 2) then
-		Heartbeat.player.x = 750
-		Heartbeat.player.y = 325
-	elseif (Player.currentLevel == 3) then
-		Heartbeat.player.x = 1925
-		Heartbeat.player.y = 275 
-	elseif (Player.currentLevel == 4) then
-		Heartbeat.player.x = 1850
-		Heartbeat.player.y = 150
-	elseif (Player.currentLevel == 5) then
-		Heartbeat.player.x = 0
-		Heartbeat.player.y = 0
-	end
 end
 
