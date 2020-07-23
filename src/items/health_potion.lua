737b52aa00570c56bd05dab784b913449d22358b
taiz <walter.kerly@protonmail.com>
2020-07-23 03:08:41 -0400

    Add health potion

---
+++ src/items/health_potion.lua
@@ -0,0 +1,19 @@
+HealthPotion = {
+	id = "health_potion",
+	name = "Health Potion",
+	texture = love.graphics.newImage("assets/tiles/ladder.png"),
+	width = 14,
+	height = 16,
+	scaleX = 1.5,
+	scaleY = 1.5
+}
+
+function HealthPotion.onPickup(this)
+	Heartbeat.player.addInventoryItem(this)
+	Heartbeat.removeItem(this)
+end
+
+function HealthPotion.onUse()
+	print("Tastes like kool aid")
+end
+
