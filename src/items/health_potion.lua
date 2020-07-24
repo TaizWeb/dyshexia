80ac41b3d64058ef093c614cf64147b6cfc12227
taiz <walter.kerly@protonmail.com>
2020-07-23 23:28:28 -0400

    Make health potions actually heal

--- src/items/health_potion.lua
+++ src/items/health_potion.lua
@@ -9,11 +9,19 @@ HealthPotion = {
 }
 
 function HealthPotion.onPickup(this)
+	-- The reason these lines need to be specified is because coins
+	-- don't actually get put into the inventory. Neither does the
+	-- ladder, which is a (technically) an item.
 	Heartbeat.player.addInventoryItem(this)
 	Heartbeat.removeItem(this)
 end
 
 function HealthPotion.onUse()
-	print("Tastes like kool aid")
+	-- This is a bit fucky since Heartbeat handles the player's health, while the Player object stores the maxHealth variable
+	if ((Heartbeat.player.health + 50) > Player.maxHealth) then
+		Heartbeat.player.health = Player.maxHealth
+	else
+		Heartbeat.player.health = Heartbeat.player.health + 50
+	end
 end
 
