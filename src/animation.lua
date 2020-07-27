439575930f66b4eee59f73e2cec55c617ea1d932
taiz <walter.kerly@protonmail.com>
2020-07-26 23:33:24 -0400

    Add optional data parameter to animations

--- src/animation.lua
+++ src/animation.lua
@@ -11,11 +11,13 @@ Animation = {
 function Animation.doAnimations()
 	for i=1,#Animation.animationList do
 		local animation = Animation.animationList[i]
+		-- Reset color
+		love.graphics.setColor(1, 1, 1, 1)
 		-- Base case for if an animation is removed, or none exist
 		if (animation == nil) then return end
 		if (animation.currentFrame < animation.frameLimit) then
 			-- If the animation is still running, pass the frame to it
-			animation.func(animation.currentFrame)
+			animation.func(animation.currentFrame, animation.data)
 			animation.currentFrame = animation.currentFrame + 1
 		else
 			-- Else, remove it
@@ -25,9 +27,10 @@ function Animation.doAnimations()
 end
 
 -- newAnimation: Adds a new animation to the list to be executed
-function Animation.newAnimation(func, frameLimit)
+function Animation.newAnimation(func, frameLimit, data)
 	Animation.animationList[#Animation.animationList + 1] = {
 		func = func,
+		data = data, -- This is an optional parameter for functions that require other data
 		frameLimit = frameLimit,
 		currentFrame = 0
 	}
